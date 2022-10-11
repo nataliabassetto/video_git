ac773115e73db49629438ce0df33cd4e374bf7c5
nataliabassetto <nataliasantos.bassetto@e2etreinamentos.com.br>
2022-10-10 21:44:37 -0300

    inclusão da mensagem Git Hub

--- projeto.git.video/src/main/java/projeto/git/video/HelloWorld.java
+++ projeto.git.video/src/main/java/projeto/git/video/HelloWorld.java
@@ -3,9 +3,9 @@ package projeto.git.video;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello World");
 
+		System.out.println("Hello World");
+		System.out.println("Git Hub");
 	}
 
 }
